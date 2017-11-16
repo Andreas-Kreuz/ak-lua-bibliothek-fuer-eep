fc07441f62050bd62f681a17b8202beac8103753
Andreas Kreuz <Andreas.Kreuz@endlich-zeit-haben.de>
2017-11-16 20:35:50 +0100

    Anlage Tutorial 2

---
+++ Resourcen/Anlagen/Andreas_Kreuz-Tutorial-Ampelkreuzung/Andreas_Kreuz-Tutorial-Ampelkreuzung-2.lua
@@ -0,0 +1,14 @@
+require("ak.demo-anlagen.tutorial-ampel.Andreas_Kreuz-Tutorial-Ampelkreuzung-2-main")
+
+
+-- Hier kommt der Code
+AkKreuzung.zeigeSignalIdsAllerSignale = true
+AkKreuzung.zeigeSchaltungAlsInfo = true
+
+[EEPLuaData]
+DS_100 = "f=0,p=Gruen,w=1,"
+DS_102 = "f=0,p=Rot,w=0,"
+DS_104 = "f=0,p=Rot,w=0,"
+DS_105 = "f=0,p=Rot,w=3,"
+DS_107 = "f=0,p=Rot,w=0,"
+DS_108 = "f=0,p=Rot,w=0,"
