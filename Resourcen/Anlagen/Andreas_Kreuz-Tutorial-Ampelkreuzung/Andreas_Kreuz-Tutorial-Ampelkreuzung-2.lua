7d54476019e2179cd63cf0a171545e8146bcc40d
Andreas Kreuz <Andreas.Kreuz@endlich-zeit-haben.de>
2017-11-24 20:36:34 +0100

    Signale zeigen nun alle Richtungen
    Neben den Schaltungen, zeigen die Signale nun alle Richtungen und deren
    Anforderungen sowie Wartezeit an.

--- Resourcen/Anlagen/Andreas_Kreuz-Tutorial-Ampelkreuzung/Andreas_Kreuz-Tutorial-Ampelkreuzung-2.lua
+++ Resourcen/Anlagen/Andreas_Kreuz-Tutorial-Ampelkreuzung/Andreas_Kreuz-Tutorial-Ampelkreuzung-2.lua
@@ -4,10 +4,11 @@ require("ak.demo-anlagen.tutorial-ampel.Andreas_Kreuz-Tutorial-Ampelkreuzung-2-m
 -- Hier kommt der Code
 AkKreuzung.zeigeSignalIdsAllerSignale = true
 AkKreuzung.zeigeSchaltungAlsInfo = true
+AkKreuzung.zeigeAnforderungenAlsInfo = true
 [EEPLuaData]
-DS_100 = "f=0,p=Rot,w=2,"
-DS_102 = "f=0,p=Gruen,w=1,"
-DS_104 = "f=0,p=Gruen,w=1,"
-DS_105 = "f=0,p=Rot,w=0,"
-DS_107 = "f=0,p=Gruen,w=1,"
-DS_108 = "f=0,p=Gruen,w=1,"
+DS_100 = "f=2,p=Gruen,w=0,"
+DS_102 = "f=0,p=Rot,w=2,"
+DS_104 = "f=0,p=Rot,w=2,"
+DS_105 = "f=0,p=Rot,w=5,"
+DS_107 = "f=0,p=Rot,w=2,"
+DS_108 = "f=0,p=Rot,w=2,"
