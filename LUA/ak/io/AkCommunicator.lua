53d4921e47a872fd9ab9aa7d34a4f964e8d3bffc
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2018-11-18 20:36:45 +0100

    Server-Synchronisation beim Schreiben der JSON-Datei.

--- LUA/ak/io/AkCommunicator.lua
+++ LUA/ak/io/AkCommunicator.lua
@@ -1,13 +1,20 @@
 AkCommunicator = {}
 
-
 local ausgabeVerzeichnis = "."
+local writing = false
+
+local function fileExists(name)
+    local f = io.open(name, "r")
+    if f ~= nil then io.close(f) return true else return false end
+end
 
 local function writeFile(dateiname, inhalt)
-    local file = io.open(dateiname, "w+")
+    local file = io.open(dateiname, "w")
     assert(file, "Kann Datei nicht öffnen " .. dateiname)
+    file:setvbuf("full", 2 * 1024 * 1024)
     io.output(file)
     io.write(inhalt)
+    io.flush()
     io.close(file)
 end
 
@@ -17,11 +24,24 @@ end
 -- @param inhalt - Dateiinhalt
 function AkCommunicator.send(type, inhalt, verzeichnis)
     local dir = verzeichnis and verzeichnis or ausgabeVerzeichnis
-    local dateiname = dir .. "/" .. "ak_out_" .. type .. ".json"
-    if not pcall(writeFile, dateiname, inhalt) then
-        print("CANNOT WRITE TO " .. dateiname)
+    if fileExists(dir .. "//ak_out_eep-web-server.on-sync-only")
+            and fileExists(dir .. "/ak_out_eep-web-server.lua-is-finished-writing-data") then
+        print("SKIPPING - server not ready")
+        return
     end
-    print("Schreibe " .. dateiname)
+
+    local dateiname = dir .. "\\" .. "ak_out_" .. type .. ".json"
+    if not writing then
+        writing = true
+        if not pcall(writeFile, dateiname, inhalt) then
+            print("CANNOT WRITE TO " .. dateiname)
+        end
+        writing = false
+    end
+
+    if fileExists(dir .. "//ak_out_eep-web-server.on-sync-only") then
+        writeFile(dir .. "/ak_out_eep-web-server.lua-is-finished-writing-data", "")
+    end
 end
 
 ---
@@ -34,7 +54,7 @@ function AkCommunicator.receive(type, verzeichnis)
     -- assert(file, "Kann Datei nicht öffnen " .. dateiname)
     -- inhalt = io.read(file)
     -- io.close(file)
-    inhalt = "Hello World!"
+    local inhalt = "Hello World!"
 
     return inhalt;
 end
