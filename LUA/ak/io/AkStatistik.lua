f705926e8c85d82c41f47de1b1a16f06bc2c77de
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2018-12-02 17:42:51 +0100

    prepare version 0.6.0

--- LUA/ak/io/AkStatistik.lua
+++ LUA/ak/io/AkStatistik.lua
@@ -6,7 +6,7 @@ local os = require("os")
 local json = require("ak.io.dkjson")
 
 local AkStatistik = {}
-AkStatistik.programVersion = "0.5.2"
+AkStatistik.programVersion = "0.6.0"
 local MAX_SIGNALS = 1000
 local MAX_TRACKS = 50000
 local MAX_STRUCTURES = 50000
