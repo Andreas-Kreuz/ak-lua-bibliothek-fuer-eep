13043aa06a2a6e2b93cd670150b866cc036cefd2
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2020-04-23 18:13:42 +0200

    remove comments

--- web-app/src/app/eep/log-viewer/log-viewer-routing.module.ts
+++ web-app/src/app/eep/log-viewer/log-viewer-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { NgxAutoScrollModule } from 'ngx-auto-scroll';
 import { LogViewerComponent } from './log-viewer/log-viewer.component';
 
 const logViewerRoutes: Routes = [
@@ -18,8 +17,7 @@ const logViewerRoutes: Routes = [
   ],
   providers: [],
   exports: [
-    RouterModule,
-    // NgxAutoScrollModule,
+    RouterModule
   ]
 })
 export class LogViewerRoutingModule {
