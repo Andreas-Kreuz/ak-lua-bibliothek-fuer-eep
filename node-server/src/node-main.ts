6fb336aec3136751e24b0b085b88e4ed798cf477
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2020-04-30 18:23:07 +0200

    add room support for json urls and settings

--- node-server/src/node-main.ts
+++ node-server/src/node-main.ts
@@ -1,7 +1,5 @@
-import CommandLineParser from './server/command-line-parser';
-import { Server } from './server/server';
+import { ServerMain } from './server/server-main';
 
 // User App Code
-const options = new CommandLineParser().parseOptions();
-const server = new Server(options['exchange-dir']);
+const server = new ServerMain();
 server.start();
---
+++ node-server/src/server/socket-event.ts
@@ -0,0 +1,4 @@
+export default class SocketEvent {
+  static JOIN_ROOM = 'join-room';
+  static LEAVE_ROOM = 'leave-room';
+}
