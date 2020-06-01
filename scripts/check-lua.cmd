c5740f69305f5fe572b38efc298d26691fb84f02
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2020-06-01 14:25:29 +0200

    change dir before luacheck and busted

--- scripts/check-lua.cmd
+++ scripts/check-lua.cmd
@@ -1,16 +1,29 @@
+@echo off
 @REM NOTE:
 @REM -----
 @REM The "luacheck" and "busted" commands must be in the PATH variable
 
-call luacheck --std max+busted lua/LUA
+setlocal
+SET oldDir=%CD%
+SET projectPath=%~dp0..
+
+@REM Run checks
+cd %projectPath%
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+call luacheck --quiet --std max+busted lua/LUA
 IF %ERRORLEVEL% NEQ 0 (
     echo %ERRORLEVEL%
     exit /b %ERRORLEVEL%
 )
-
 
 call busted --verbose --coverage -o plainTerminal --
 IF %ERRORLEVEL% NEQ 0 (
     echo %ERRORLEVEL%
     exit /b %ERRORLEVEL%
 )
+
+cd %oldDir%
+endlocal
