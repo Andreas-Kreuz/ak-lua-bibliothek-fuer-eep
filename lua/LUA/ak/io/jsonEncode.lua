92ebda8c5a2730fa2b923aa43fa5f33f881451da
FrankBuchholz <Frank.Buchholz@web.de>
2021-03-25 12:50:26 +0100

    Performance improvement: simplified json.encode

--- lua/LUA/ak/io/jsonEncode.lua
+++ lua/LUA/ak/io/jsonEncode.lua
@@ -1,13 +1,13 @@
 if AkDebugLoad then print("Loading ak.io.jsonEncode ...") end
 --
--- jsonEncode.lua 
+-- jsonEncode.lua
 -- Simplified and fast version omitting any checks
 
 local json = { _version = "1.0.0" }
 
 -- local alias names increase performance
-local type, tostring, pairs, ipairs, next, table_insert, table_concat
-    = type, tostring, pairs, ipairs, next, table.insert, table.concat
+local type, tostring, pairs, ipairs, next, table_concat --, table_insert
+    = type, tostring, pairs, ipairs, next, table.concat --, table.insert
 
 -------------------------------------------------------------------------------
 -- Encode
@@ -21,35 +21,35 @@ local result = {}
 local function cat(str)
 -- table_insert(result, str)    -- same speed
     result[#result + 1] = str    -- as this command
-end    
+end
 
 -- replace last entry
 local function rep(str)
     result[#result] = str
-end    
+end
 
 local function encodeTable(tab)
     if tab[1] ~= nil then
         -- Encode array
         cat( "[" )
-        for i, value in ipairs(tab) do
+        for _, value in ipairs(tab) do
             encode(value)
             cat( "," )
         end
         rep( "]" ) -- replace last colon
         return
-        
-    elseif next(tab) ~= nil then    
+
+    elseif next(tab) ~= nil then
         -- Encode object
          cat( "{" )
         for key, value in pairs(tab) do
             --encode(key)
             --cat( ":" )
             --encode(value)
-            -- It's faster to skip the last level of recursion 
+            -- It's faster to skip the last level of recursion
             if     type(value) == "string" then
                 cat( string.format('"%s":"%s"', key, value) )
-                
+
             elseif type(value) == "number" then -- Differentiate between integer and float
                 cat( string.format(( value == math.floor(value) and '"%s":%d' or '"%s":%.3f' ), key, value) )
             else
@@ -60,7 +60,7 @@ local function encodeTable(tab)
         end
         rep( "}" ) -- replace last colon
         return
-        
+
     else
         -- Encode empty table
         cat( "[]" )
@@ -105,7 +105,7 @@ function json.encode(value)
     result = {}
 
 local t0 = os.clock()
-    
+
     encode(value) -- this is the slow part
 
 local t1 = os.clock()
@@ -113,10 +113,10 @@ local t1 = os.clock()
     local jsonString = table_concat(result)    -- this is very fast
 
 local t2 = os.clock()
-print(string.format("json.Encode: concat %d lines into %d characters, encode in %.3f sec, concat in %.3f sec", 
+print(string.format("json.Encode: concat %d lines into %d characters, encode in %.3f sec, concat in %.3f sec",
     #result, #jsonString, t1-t0, t2-t1
 ))
-    
+
     return jsonString
 end
 
