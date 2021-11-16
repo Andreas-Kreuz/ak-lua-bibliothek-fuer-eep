dd66a424e05f2c396e8ff971e7dba5d325572d54
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-11-16 22:27:16 +0100

    show speed of zero on card

--- web-app/src/app/eep/trains/info-speed/info-speed.component.ts
+++ web-app/src/app/eep/trains/info-speed/info-speed.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info-speed',
-  template: `<span *ngIf="speed"><mat-icon>speed</mat-icon>&nbsp;{{ speed }} km/h</span>`,
+  template: `<span *ngIf="speed === 0 || speed"><mat-icon>speed</mat-icon>&nbsp;{{ speed }} km/h</span>`,
   styles: [],
 })
 export class InfoSpeedComponent implements OnInit {
