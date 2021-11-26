053e5d7e4e2e4afacb44bbb478d0fbb08b0097fb
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-11-26 18:13:38 +0100

    fix counter

--- web-app/src/app/server/server-status/server-status.component.ts
+++ web-app/src/app/server/server-status/server-status.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import * as fromServer from '../store/server.reducer';
-import * as ServerAction from '../store/server.actions';
 import { Observable } from 'rxjs';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EepDirDialogComponent } from './eep-dir-dialog.component';
 import { registerLocaleData } from '@angular/common';
-import localeFr from '@angular/common/locales/de';
+import localeDe from '@angular/common/locales/de';
 
 @Component({
   selector: 'app-server-status',
@@ -29,12 +28,14 @@ export class ServerStatusComponent implements OnInit {
     this.eepDirOk$ = this.store.select(fromServer.eepDirOk$);
     this.urls$ = this.store.select(fromServer.urls$);
     this.urlsAvailable$ = this.store.select(fromServer.urlsAvailable$);
+    this.eventCounter$ = this.store.select(fromServer.selectEventCounter$);
     this.eepDirOk$.subscribe((ok) => {
       this.error = !ok;
     });
     this.eepDir$.subscribe((dir) => {
       this.dir = dir;
     });
+    registerLocaleData(localeDe);
   }
 
   openDialog(): void {
