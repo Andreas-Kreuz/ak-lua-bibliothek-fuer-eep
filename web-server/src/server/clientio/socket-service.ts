d0a88fa36e4a26d2234e727f71e8e339bca696f8
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-11-28 11:16:34 +0100

    harmonize debug output

--- web-server/src/server/clientio/socket-service.ts
+++ web-server/src/server/clientio/socket-service.ts
@@ -21,12 +21,12 @@ export default class SocketService {
 
       socket.on(RoomEvent.JoinRoom, (room: { room: string }) => {
         socket.join(room.room);
-        if (this.debug) console.log(socket.id + ' joined rooms: "' + room.room + '" ');
+        if (this.debug) console.log('JOIN ' + room.room + ' from ' + socket.id);
       });
 
       socket.on(RoomEvent.LeaveRoom, (room: { room: string }) => {
         socket.leave(room.room);
-        if (this.debug) console.log(socket.id + ' left room: "' + room.room + '" ');
+        if (this.debug) console.log('LEFT ' + room.room + ' by ' + socket.id);
       });
     });
   }
