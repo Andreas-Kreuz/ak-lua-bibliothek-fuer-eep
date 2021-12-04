9d7a972bbb8c72a88ec9801ae7c9f2d48787657c
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-12-04 12:42:31 +0100

    add sample cards to public transport

--- web-app/src/app/client/modules/public-transport/public-transport.module.ts
+++ web-app/src/app/client/modules/public-transport/public-transport.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { PublicTransportRoutingModule } from './public-transport-routing.module';
 import { PublicTransportComponent } from './public-transport.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    PublicTransportComponent
-  ],
-  imports: [
-    CommonModule,
-    PublicTransportRoutingModule
-  ]
+  declarations: [PublicTransportComponent],
+  imports: [CommonModule, MatCardModule, MatDividerModule, MatListModule, PublicTransportRoutingModule],
 })
-export class PublicTransportModule { }
+export class PublicTransportModule {}
