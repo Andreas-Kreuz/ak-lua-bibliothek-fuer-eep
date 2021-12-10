a553a9c232959ba6dcded512b145d2ca42e36720
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-12-10 15:31:32 +0100

    prepare public transport sample page

--- web-app/src/app/client/modules/public-transport/public-transport.module.ts
+++ web-app/src/app/client/modules/public-transport/public-transport.module.ts
@@ -6,9 +6,17 @@ import { PublicTransportComponent } from './public-transport.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PublicTransportComponent],
-  imports: [CommonModule, MatCardModule, MatDividerModule, MatListModule, PublicTransportRoutingModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    PublicTransportRoutingModule,
+  ],
 })
 export class PublicTransportModule {}
