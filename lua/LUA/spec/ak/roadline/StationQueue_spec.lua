e1d53d766a62beb105282ef2f5a9682dafc92804
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-12-12 20:40:06 +0100

    fix train and rollingstock assigment

--- lua/LUA/spec/ak/roadline/StationQueue_spec.lua
+++ lua/LUA/spec/ak/roadline/StationQueue_spec.lua
@@ -1,8 +1,8 @@
 describe("ak.public-transport.StationQueue", function()
     local EepSimulator = require("ak.core.eep.EepSimulator")
     EepSimulator.addTrain("train1", "RollingStock 1a", "RollingStock 2b")
-    EepSimulator.addTrain("train2", "RollingStock 1a", "RollingStock 2b")
-    EepSimulator.addTrain("train3", "RollingStock 1a", "RollingStock 2b")
+    EepSimulator.addTrain("train2", "RollingStock 2a", "RollingStock 2b")
+    EepSimulator.addTrain("train3", "RollingStock 3a", "RollingStock 3b")
 
     insulate("new Queue is empty", function()
         local Queue = require("ak.public-transport.StationQueue")
