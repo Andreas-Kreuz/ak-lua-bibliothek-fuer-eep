c0d6723527f01ae8c13b0c870f8de9133dcace9b
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-12-14 19:48:15 +0100

    add script for manual testing

---
+++ scripts/check-web-app-e2e.cmd
@@ -0,0 +1,33 @@
+@REM NOTE:
+@REM -----
+@REM The "npm" command must be in the PATH variable
+@REM Requires node.js to be installed (https://nodejs.org/en/).
+
+setlocal
+SET oldDir=%CD%
+SET projectPath=%~dp0..
+
+@REM Build EEP Web Shared
+cd %projectPath%\web-shared
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+call npm run-script build
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+@REM Build EEP Web App
+cd %projectPath%\web-app
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+call npm run-script cy-tests-run
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+cd %oldDir%
+endlocal
