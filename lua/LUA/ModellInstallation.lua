e04cbade609bbf1e9aa8cfaa7fed214dd3f67063
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2021-12-18 07:37:55 +0100

    add lines demo to the installer

--- lua/LUA/ModellInstallation.lua
+++ lua/LUA/ModellInstallation.lua
@@ -5,27 +5,17 @@ local AkModellInstaller = require("ak.modellpacker.AkModellInstaller")
 local AkModellPaket = require("ak.modellpacker.AkModellPaket")
 
 local aktuellerOrdner = ".."
-if arg and arg[1] then
-    aktuellerOrdner = arg[1]
-end
+if arg and arg[1] then aktuellerOrdner = arg[1] end
 print("Suche Installationsdateien in Verzeichnis \"" .. aktuellerOrdner .. "\"")
-
 
 -----------------------------------------
 -- Paket: Skripte von Andreas Kreuz
 -----------------------------------------
 local paket0 = AkModellPaket:new("13,2", "Lua-Bibliothek von Andreas Kreuz",
-    "Lua-Bibliothek mit Verkehrssteuerung, Aufgabenplanung und Modell-Installation")
+                                 "Lua-Bibliothek mit Verkehrssteuerung, Aufgabenplanung und Modell-Installation")
 paket0:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak", {
-    "README.md",
-    "ak-eep-in.commands",
-    "ak-eep-out.json",
-    "ak-eep-out-json.isfinished",
-    "ak-eep-out.log",
-    "ak-eep-out.socket",
-    "ak-eep-version.txt",
-    "ak-server.iswatching",
-    "anlagen",
+    "README.md", "ak-eep-in.commands", "ak-eep-out.json", "ak-eep-out-json.isfinished", "ak-eep-out.log",
+    "ak-eep-out.socket", "ak-eep-version.txt", "ak-server.iswatching", "anlagen"
 })
 
 do
@@ -37,38 +27,28 @@ end
 -----------------------------------------
 -- Paket: Demo-Anlage Ampelkreuzung
 -----------------------------------------
-local paket1 = AkModellPaket:new("13,2", "Demo-Anlage Ampel (Grundmodelle)",
-        "Eine Anlage mit Grundmodellen aus EEP 14 - mit zwei Kreuzungen und Ampel-Skripten")
-paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\ampel", {
-    "README.md"
-})
-paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Ampel", {
-    ".dds", "README.md"
-})
+local paket1 = AkModellPaket:new("13,2", "Demo-Anlage (Ampel, ÖPNV)", "Die Demo-Anlagen für Ampeln und ÖPNV")
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\ampel", {"README.md"})
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Ampel", {".dds", "README.md"})
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\demo-linien", {".dds", "README.md"})
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Linien", {".dds", "README.md"})
 
 -----------------------------------------
 -- Paket: Demo-Anlage testen
 -----------------------------------------
 local paket2 = AkModellPaket:new("13,2", "Demo-Anlage Testen mit EEP (Erweiterte Modelle)",
-        "Eine Anlage mit Shop-Modellen - mit zwei komplexen Kreuzungen und Ampel-Skripten")
-paket2:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\testen", {
-    "README.md"
-})
-paket2:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Testen", {
-    ".dds", "README.md"
-})
+                                 "Eine Anlage mit Shop-Modellen - mit zwei komplexen Kreuzungen und Ampel-Skripten")
+paket2:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\testen", {"README.md"})
+paket2:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Testen", {".dds", "README.md"})
 
 -----------------------------------------
 -- Paket: Tutorial Ampelkreuzung
 -----------------------------------------
 local paket3 = AkModellPaket:new("13,2", "Tutorial - Aufbau einer Ampelkreuzung",
-        "Eine Anlage mit einer Kreuzung, die die Verwendung der Lua-Bibliothek erklÃ¤rt")
-paket3:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\tutorial-ampel", {
-    "README.md"
-})
-paket3:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Tutorial-Ampelkreuzung", {
-    ".dds", "README.md"
-})
+                                 "Eine Anlage mit einer Kreuzung, die die Verwendung der Lua-Bibliothek erklärt")
+paket3:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\tutorial-ampel", {"README.md"})
+paket3:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Tutorial-Ampelkreuzung",
+                         {".dds", "README.md"})
 
 print(paket0.deutscherName)
 print(paket1.deutscherName)
