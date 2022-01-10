1c3a8c46c60373dc1c73e11ab5159c7446d4c22a
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2022-01-10 22:47:35 +0100

    give log test more time for event parsing

--- web-app/cypress/integration/modules/log/log_spec.ts
+++ web-app/cypress/integration/modules/log/log_spec.ts
@@ -4,58 +4,72 @@ const simulator = new EepSimulator();
 
 beforeEach(() => {
   simulator.reset();
-  cy.wait(100); // Give the web server some time to remove the previous log lines
+  cy.wait(1000); // Give the web server some time to remove the previous log lines
 });
 
 describe('Logger', () => {
   it('has no initial log', () => {
     cy.visit('/log');
-    cy.get('ul.list-pre').children().should('have.length', 0);
+    cy.wait(500).then(() => {
+      cy.readFile(simulator.fileNames.eepOutLog).then((a) => cy.log(a));
+      cy.get('ul.list-pre').children().should('have.length', 0);
+    });
   });
 
   describe('displays', () => {
     it('log line "Let us test something"', () => {
       simulator.writeLogLine('Let us test something');
       cy.visit('/log');
-      cy.get('ul.list-pre').children().should('have.length', 1).first().contains('Let us test something');
+      cy.wait(500).then(() => {
+        cy.readFile(simulator.fileNames.eepOutLog).then((a) => cy.log(a));
+        cy.get('ul.list-pre').children().should('have.length', 1).first().contains('Let us test something');
+      });
     });
 
     it('latin1 characters correctly: "Äpfel, Überschuss, ÖPNV"', () => {
       simulator.writeLogLine('Äpfel, Überschuss, ÖPNV');
       cy.visit('/log');
-      cy.get('ul.list-pre').children().should('have.length', 1).first().contains('Äpfel, Überschuss, ÖPNV');
+      cy.wait(500).then(() => {
+        cy.readFile(simulator.fileNames.eepOutLog).then((a) => cy.log(a));
+        cy.get('ul.list-pre').children().should('have.length', 1).first().contains('Äpfel, Überschuss, ÖPNV');
+      });
     });
 
     it('displays log lines 1 to 3', () => {
-      cy.wait(100);
       simulator.writeLogLine('Line 1\nLine 2\nLine 3');
       cy.visit('/log');
-      cy.get('ul.list-pre')
-        .children()
-        .should('have.length', 3)
-        .first()
-        .contains('Line 1')
-        .next()
-        .contains('Line 2')
-        .next()
-        .contains('Line 3');
+      cy.wait(500).then(() => {
+        cy.readFile(simulator.fileNames.eepOutLog).then((a) => cy.log(a));
+        cy.get('ul.list-pre')
+          .children()
+          .should('have.length', 3)
+          .first()
+          .contains('Line 1')
+          .next()
+          .contains('Line 2')
+          .next()
+          .contains('Line 3');
+      });
     });
 
     it('displays log lines 1 to 4', () => {
       simulator.writeLogLine('Line 1\nLine 2\nLine 3');
       simulator.writeLogLine('Line 4');
       cy.visit('/log');
-      cy.get('ul.list-pre')
-        .children()
-        .should('have.length', 4)
-        .first()
-        .contains('Line 1')
-        .next()
-        .contains('Line 2')
-        .next()
-        .contains('Line 3')
-        .next()
-        .contains('Line 4');
+      cy.wait(500).then(() => {
+        cy.readFile(simulator.fileNames.eepOutLog).then((a) => cy.log(a));
+        cy.get('ul.list-pre')
+          .children()
+          .should('have.length', 4)
+          .first()
+          .contains('Line 1')
+          .next()
+          .contains('Line 2')
+          .next()
+          .contains('Line 3')
+          .next()
+          .contains('Line 4');
+      });
     });
   });
   describe('action', () => {
