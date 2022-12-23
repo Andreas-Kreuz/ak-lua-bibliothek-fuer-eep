4eeb4b3f75d33061980f1a269d0a985c1f80db8e
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2022-12-23 08:17:23 +0100

    Add tsx formatter and format react files

--- web-app-react/src/client/Client.tsx
+++ web-app-react/src/client/Client.tsx
@@ -1,31 +1,31 @@
-import "./Client.css";
-import React, { useState, useEffect } from "react";
-import io from "socket.io-client";
+import './Client.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-const socket = io("localhost:3000");
+const socket = io('localhost:3000');
 
 function Client() {
   const [isConnected, setIsConnected] = useState(socket.connected);
 
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("Connected to socket!");
+    socket.on('connect', () => {
+      console.log('Connected to socket!');
       setIsConnected(true);
     });
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       setIsConnected(false);
     });
 
     return () => {
-      socket.off("connect");
-      socket.off("disconnect");
+      socket.off('connect');
+      socket.off('disconnect');
     };
   }, []);
 
   return (
     <div className="Client">
       <h1>Client</h1>
-      <p>Connected: {"" + isConnected}</p>
+      <p>Connected: {'' + isConnected}</p>
     </div>
   );
 }
--- web-app-react/src/server/Server.tsx
+++ web-app-react/src/server/Server.tsx
@@ -1,31 +1,31 @@
-import "./Server.css";
-import React, { useState, useEffect } from "react";
-import io from "socket.io-client";
+import './Server.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-const socket = io("localhost:3000");
+const socket = io('localhost:3000');
 
 function Server() {
   const [isConnected, setIsConnected] = useState(socket.connected);
 
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("Connected to socket!");
+    socket.on('connect', () => {
+      console.log('Connected to socket!');
       setIsConnected(true);
     });
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       setIsConnected(false);
     });
 
     return () => {
-      socket.off("connect");
-      socket.off("disconnect");
+      socket.off('connect');
+      socket.off('disconnect');
     };
   }, []);
 
   return (
     <div className="Server">
       <h1>Server</h1>
-      <p>Connected: {"" + isConnected}</p>
+      <p>Connected: {'' + isConnected}</p>
     </div>
   );
 }
