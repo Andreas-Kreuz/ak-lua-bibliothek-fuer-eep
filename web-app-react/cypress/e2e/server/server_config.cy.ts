1a57553dede33a10e13a750129b47eceff84be11
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2022-12-28 16:45:39 +0100

    add cypress tests (still not working)

---
+++ web-app-react/cypress/e2e/server/server_config.cy.ts
@@ -0,0 +1,156 @@
+import EepSimulator from '../../test-helpers/eep-simulator';
+
+const simulator = new EepSimulator();
+const DIRECTORY = 'ANOTHER_DIRECTORY';
+
+describe('Server Tests "/server"', () => {
+  const pwd: { dir: string } = { dir: undefined };
+
+  before(() => {
+    // Remember the old server dir otherwise the following tests will not work!
+    cy.visit('/server');
+    cy.wait(500).then(() => {
+      cy.get('#choose-dir-current-dir')
+        .should('not.have.text', '-')
+        .should('not.contain.text', 'io-empty')
+        .invoke('text')
+        .then((value) => {
+          pwd.dir = value as string;
+        });
+    });
+    simulator.reset();
+    simulator.eepEvent('eep-version-complete.json');
+    cy.wait(500).then(() => {
+      cy.contains('api-entries');
+      cy.contains('eep-version');
+      cy.contains('(2 events)');
+    });
+  });
+
+  after(() => {
+    if (pwd.dir) {
+      // Reset the old server dir otherwise the following tests will not work!
+      cy.log('RESET TO: ' + pwd.dir);
+      cy.visit('/server');
+      cy.wait(500);
+      cy.get('#choose-dir-current-dir')
+        .should('not.have.text', '-')
+        .then(() => {
+          cy.get('#choose-dir-button')
+            .should('be.enabled')
+            .click()
+            .then(() => {
+              cy.get('input#dir-dialog-dir')
+                .should('exist')
+                .should('be.visible')
+                .wait(100)
+                .clear()
+                .type(pwd.dir)
+                .then((bla) => {
+                  cy.get('#dir-dialog-choose').click().should('not.exist');
+                });
+            });
+        });
+    }
+  });
+
+  it('has button "Verzeichnis wählen"', () => {
+    cy.get('#choose-dir-button');
+  });
+  it('contains "Happily Serving"', () => {
+    cy.contains('Happily Serving');
+  });
+  it('contains "(2 events)"', () => {
+    cy.contains('(2 events)');
+  });
+
+  describe('Changing the directory', () => {
+    it('button "Wählen" is enabled', () => {
+      cy.get('#choose-dir-button').click();
+      cy.get('input#dir-dialog-dir').should('be.visible').should('contain.value', pwd.dir);
+      cy.get('#dir-dialog-choose').should('be.enabled');
+      cy.get('#dir-dialog-cancel').should('be.enabled');
+      cy.get('#dir-dialog-cancel').should('be.enabled').click().should('not.exist');
+    });
+    it('button "Wählen" is disabled', () => {
+      cy.get('#choose-dir-button').click();
+      cy.get('input#dir-dialog-dir')
+        .should('be.visible')
+        .should('contain.value', 'io')
+        .wait(100)
+        .clear()
+        .type('{selectall}{backspace}')
+        .then(() => {
+          cy.get('#dir-dialog-choose')
+            .should('be.disabled')
+            .then(() => {
+              cy.get('#dir-dialog-cancel').should('be.enabled');
+              cy.get('#dir-dialog-cancel').should('be.enabled').click().should('not.exist');
+            });
+        });
+    });
+    describe('Changing to "bad" directory', () => {
+      it('Change to non-existing directory error', () => {
+        cy.get('#choose-dir-button')
+          .should('be.enabled')
+          .click()
+          .then(() => {
+            cy.get('input#dir-dialog-dir')
+              .wait(100)
+              .clear()
+              .type('non-existing')
+              .then(() => {
+                cy.get('#dir-dialog-choose').click().should('not.exist');
+                cy.wait(1000);
+                cy.contains(
+                  'Bitte gib das Verzeichnis Deiner EEP-Installation an. ' +
+                    'Die Lua-Bibliothek muss installiert sein. ' +
+                    'Der Server sucht nach dem Verzeichnis LUA/ak/io/exchange.'
+                );
+              });
+          });
+      });
+
+      it('Change to EEP directory without contents ', () => {
+        cy.get('#choose-dir-button')
+          .should('be.enabled')
+          .click()
+          .then(() => {
+            cy.get('input#dir-dialog-dir')
+              .should('exist')
+              .should('be.visible')
+              .wait(100)
+              .clear()
+              .type(pwd.dir + '-empty')
+              .then(() => {
+                cy.get('#dir-dialog-choose').click().should('not.exist');
+                cy.wait(1000);
+                cy.contains('Es wurden keine Daten von EEP gesammelt');
+              });
+          });
+      });
+
+      after(() => {
+        // Reset the old server dir otherwise the following tests will not work!
+        cy.get('#choose-dir-current-dir')
+          .should('contain.text', '-empty')
+          .then(() => {
+            cy.get('#choose-dir-button')
+              .should('be.enabled')
+              .click()
+              .then(() => {
+                cy.get('input#dir-dialog-dir')
+                  .should('exist')
+                  .should('be.visible')
+                  .wait(100)
+                  .clear()
+                  .type(pwd.dir)
+                  .then(() => {
+                    cy.get('#dir-dialog-choose').click().should('not.exist');
+                  });
+              });
+          });
+      });
+    });
+  });
+});
---
+++ web-app-react/cypress/test-helpers/eep-simulator.ts
@@ -0,0 +1,66 @@
+enum FileNames {
+  eepOutJsonOut = 'cypress/io/LUA/ak/io/exchange/ak-eep-out.json',
+  eepOutJsonOutFinished = 'cypress/io/LUA/ak/io/exchange/ak-eep-out-json.isfinished',
+  eepOutLog = 'cypress/io/LUA/ak/io/exchange/ak-eep-out.log',
+  serverOutCommands = 'cypress/io/LUA/ak/io/exchange/ak-eep-in.commands',
+  serverWatching = 'cypress/io/LUA/ak/io/exchange/ak-server.iswatching',
+}
+
+export default class EepSimulator {
+  fileNames = FileNames;
+  private eventCounter = 0;
+
+  logEventCounter = () => cy.log('Current Counter: ' + this.eventCounter.toString());
+
+  reset = () => {
+    this.eventCounter = 0;
+    cy.readFile(FileNames.serverWatching).should('exist');
+    cy.writeFile(FileNames.eepOutLog, '');
+    cy.writeFile(FileNames.serverOutCommands, '');
+    this.eepEvent('reset.json');
+  };
+
+  loadFixtures = (fixtures: string[]) => {
+    const res = [];
+    // could also use `res.push(f)` they should be equivalent
+    fixtures.map((name, i) => cy.fixture(name).then((f) => (res[i] = f)));
+    return cy.wrap(res);
+  };
+
+  simulateMap(mapName: string, startEvent: number, endEvent: number) {
+    const eventFileNames = [];
+    for (let i = startEvent; i <= endEvent; i++) {
+      const name = mapName + '/eep-event' + i + '.json';
+      eventFileNames.push(name);
+    }
+    const eventJsons = this.loadFixtures(eventFileNames).then((jsons) => {
+      cy.log(jsons.length.toLocaleString());
+      cy.writeFile(FileNames.serverOutCommands, '');
+      this.writeNewEepEventFile(jsons.map((x) => JSON.stringify(x)).join('\n'));
+    });
+  }
+
+  eepEvent(fileName: string) {
+    this.eventCounter++;
+    const count = this.eventCounter;
+    cy.fixture('eep-output/' + fileName).then((x) => {
+      x.eventCounter = count;
+      cy.log(x.eventCounter, fileName, x);
+      this.writeNewEepEventFile(JSON.stringify(x));
+    });
+  }
+
+  writeLogLine(line: string) {
+    cy.readFile(FileNames.eepOutLog, 'latin1').then((oldLines) => {
+      cy.log(oldLines);
+      cy.writeFile(FileNames.eepOutLog, oldLines + '\n' + line, 'latin1');
+      cy.wait(100); // Give the web server some time to read new log lines
+    });
+  }
+
+  private writeNewEepEventFile(eventLines: string) {
+    cy.writeFile(FileNames.eepOutJsonOut, eventLines, 'latin1');
+    cy.writeFile(FileNames.eepOutJsonOutFinished, '');
+    cy.readFile(FileNames.eepOutJsonOutFinished).should('not.exist');
+  }
+}
