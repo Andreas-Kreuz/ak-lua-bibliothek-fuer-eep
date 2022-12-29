52c5e6290e854118bb630f4487d1d24a6ddc7216
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2022-12-29 12:35:03 +0100

    fix server tests

--- web-app-react/cypress/e2e/server/server.cy.ts
+++ web-app-react/cypress/e2e/server/server.cy.ts
@@ -10,16 +10,16 @@ describe('Server Home', () => {
   it('has 1 event and api-entries after reset', () => {
     cy.visit('/server');
     cy.contains('Server');
-    cy.contains('Web-App im Browser öffnen');
-    cy.contains('Happily Serving');
-    cy.contains('(1 events)');
+    cy.contains('App öffnen');
+    cy.contains('Bereitgestellte Daten');
+    cy.contains('aus 1 Events');
     cy.contains('api-entries');
   });
 
   it('has 2 events and contains eep-version after second event', () => {
     simulator.eepEvent('eep-version-complete.json');
     cy.visit('/server');
-    cy.contains('(2 events)');
+    cy.contains('aus 2 Events');
     cy.contains('api-entries');
     cy.contains('eep-version');
   });
