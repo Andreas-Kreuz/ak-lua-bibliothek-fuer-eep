a113f9bfd38a1a12165b8b0f9a8128a49263a762
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2022-12-29 11:20:03 +0100

    Do not isolate tests
    This is needed to reuse the same page on server tests.

--- web-app-react/cypress.config.ts
+++ web-app-react/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
