89fed135119cc413c86a77a1e50d7cd55efe3fda
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2023-02-12 17:08:39 +0100

    restrict main menu to intersections

--- web-app-react/src/app/MainMenu.tsx
+++ web-app-react/src/app/MainMenu.tsx
@@ -3,6 +3,7 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
+import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
 import useNavState from './NavElements';
 
@@ -17,11 +18,22 @@ function MainMenu() {
     <Grid container sx={{ p: 2, width: '100vw' }} spacing={2}>
       {trafficNav.map((card) => (
         <Grid xs={12} sm={6} lg={3} key={card.title}>
-          <Card elevation={3}>
+          <Card elevation={3} sx={{ borderRadius: 2 }}>
             <CardActionArea>
-              {card.image ? <CardMedia component="img" image={'/assets/' + card.image} title="green iguana" /> : ''}
-              <CardHeader title={card.title} subheader={card.subtitle}></CardHeader>
-              <CardContent>Hallo Echo</CardContent>
+              <Stack sx={{ flexDirection: { xs: 'row', sm: 'column' } }}>
+                {card.image ? (
+                  <CardMedia
+                    component="img"
+                    image={'/assets/' + card.image}
+                    title={card.title}
+                    sx={{ width: { xs: '25%', sm: 1 }, borderRadius: 2 }}
+                  />
+                ) : (
+                  ''
+                )}
+                <CardHeader title={card.title} subheader={card.subtitle}></CardHeader>
+              </Stack>
+              {/* <CardContent>Hallo Echo</CardContent> */}
             </CardActionArea>
           </Card>
         </Grid>
