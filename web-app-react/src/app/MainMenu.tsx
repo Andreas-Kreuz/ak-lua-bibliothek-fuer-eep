eb7e8b42e7039f6f4673f2e6eab1b4351e2f9d18
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2023-02-12 14:48:13 +0100

    add main menu images

--- web-app-react/src/app/MainMenu.tsx
+++ web-app-react/src/app/MainMenu.tsx
@@ -1,13 +1,9 @@
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Typography,
-  Unstable_Grid2 as Grid,
-} from '@mui/material';
-import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Unstable_Grid2';
 import useNavState from './NavElements';
 
 function MainMenu() {
@@ -15,15 +11,15 @@ function MainMenu() {
 
   const trafficNav = navigation.filter((nav) => nav.name === 'Verkehr').flatMap((nav) => nav.values);
   const dataNav = navigation.filter((nav) => nav.name === 'Daten').flatMap((nav) => nav.values);
-  console.log(trafficNav);
+  console.log(trafficNav[0].image);
 
   return (
     <Grid container sx={{ p: 2, width: '100vw' }} spacing={2}>
       {trafficNav.map((card) => (
         <Grid xs={12} sm={6} lg={3} key={card.title}>
-          <Card>
+          <Card elevation={3}>
             <CardActionArea>
-              {card.image ? <CardMedia image="{card.image}" title="green iguana" /> : ''}
+              {card.image ? <CardMedia component="img" image={'/assets/' + card.image} title="green iguana" /> : ''}
               <CardHeader title={card.title} subheader={card.subtitle}></CardHeader>
               <CardContent>Hallo Echo</CardContent>
             </CardActionArea>
