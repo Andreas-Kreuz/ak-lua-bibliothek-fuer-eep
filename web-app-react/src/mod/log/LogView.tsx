c97972f55d257594a8c803e3224db5db3bd1679d
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2023-02-19 14:40:37 +0100

    remove unneccessary transition group

--- web-app-react/src/mod/log/LogView.tsx
+++ web-app-react/src/mod/log/LogView.tsx
@@ -36,82 +36,80 @@ function LogView() {
 
   return (
     <Box>
-      <TransitionGroup>
-        <Paper
-          variant="elevation"
-          elevation={20}
+      <Paper
+        variant="elevation"
+        elevation={20}
+        sx={{
+          borderTopLeftRadius: 8,
+          borderTopRightRadius: 8,
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
+          borderBottomWidth: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          position: 'fixed',
+          bottom: 0,
+          right: 0,
+          m: 0,
+          mr: open ? 0 : 2,
+          p: 0,
+          maxHeight: open ? 'calc(100vh - 60px)' : '150px',
+          transition: theme.transitions.create(['max-width', 'max-height', 'margin-right'], transitionOptions),
+        }}
+      >
+        <Box
           sx={{
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-            borderBottomLeftRadius: 0,
-            borderBottomRightRadius: 0,
-            borderBottomWidth: 0,
             display: 'flex',
-            flexDirection: 'column',
+            alignItems: 'center',
             justifyContent: 'flex-end',
-            position: 'fixed',
-            bottom: 0,
-            right: 0,
-            m: 0,
-            mr: open ? 0 : 2,
-            p: 0,
-            maxHeight: open ? 'calc(100vh - 60px)' : '150px',
-            transition: theme.transitions.create(['max-width', 'max-height', 'margin-right'], transitionOptions),
+            mr: open ? 2 : 0,
+            transition: theme.transitions.create(['margin-right'], transitionOptions),
           }}
         >
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              mr: open ? 2 : 0,
-              transition: theme.transitions.create(['margin-right'], transitionOptions),
-            }}
-          >
-            {open && (
-              <>
-                <Typography variant="body1" sx={{ px: 1, py: 0.5 }}>
-                  EEP Log
-                </Typography>
+          {open && (
+            <>
+              <Typography variant="body1" sx={{ px: 1, py: 0.5 }}>
+                EEP Log
+              </Typography>
 
-                <Box sx={{ flexGrow: 1 }} />
-                <Button
-                  variant="text"
-                  startIcon={<DeleteIcon />}
-                  onClick={() => {
-                    clearLog();
-                  }}
-                  sx={{ m: 0.5 }}
-                  disableRipple
-                >
-                  Log löschen
-                </Button>
-              </>
-            )}
-            <Button
-              variant="text"
-              startIcon={open ? <ArrowDownIcon /> : <ArrowUpIcon />}
-              onClick={() => setOpen(!open)}
-              sx={{ m: 0.5 }}
-              disableRipple
-            >
-              {open ? 'Log verbergen' : 'Log anzeigen'}
-            </Button>
-          </Box>
-          <Box
-            height={open ? '14em' : 0}
-            width={open ? 'calc(100vw)' : 0}
-            sx={{
-              overflow: 'auto',
-              px: open ? 1 : 0,
-              transition: theme.transitions.create(['width', 'height'], transitionOptions),
-            }}
+              <Box sx={{ flexGrow: 1 }} />
+              <Button
+                variant="text"
+                startIcon={<DeleteIcon />}
+                onClick={() => {
+                  clearLog();
+                }}
+                sx={{ m: 0.5 }}
+                disableRipple
+              >
+                Log löschen
+              </Button>
+            </>
+          )}
+          <Button
+            variant="text"
+            startIcon={open ? <ArrowDownIcon /> : <ArrowUpIcon />}
+            onClick={() => setOpen(!open)}
+            sx={{ m: 0.5 }}
+            disableRipple
           >
-            {open && <Divider sx={{ mb: 1 }} />}
-            {open && <LogLinesView />}
-          </Box>
-        </Paper>
-      </TransitionGroup>
+            {open ? 'Log verbergen' : 'Log anzeigen'}
+          </Button>
+        </Box>
+        <Box
+          height={open ? '14em' : 0}
+          width={open ? 'calc(100vw)' : 0}
+          sx={{
+            overflow: 'auto',
+            px: open ? 1 : 0,
+            transition: theme.transitions.create(['width', 'height'], transitionOptions),
+          }}
+        >
+          {open && <Divider sx={{ mb: 1 }} />}
+          {open && <LogLinesView />}
+        </Box>
+      </Paper>
     </Box>
   );
 }
