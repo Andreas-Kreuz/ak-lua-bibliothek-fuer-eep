5661d4919a3befbf88da3f47b076be63662a3e31
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2023-02-19 18:21:34 +0100

    add script for webserver start after rebuild

---
+++ scripts/start-web-server-after-rebuild.cmd
@@ -0,0 +1,22 @@
+@REM NOTE:
+@REM -----
+@REM The "npm" command must be in the PATH variable
+@REM Requires node.js to be installed (https://nodejs.org/en/).
+
+setlocal
+SET oldDir=%CD%
+SET projectPath=%~dp0..
+
+@REM Build EEP Web App
+cd %projectPath%\web-server
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+call npm run-script start-with-app
+IF %ERRORLEVEL% NEQ 0 (
+   exit /b %ERRORLEVEL%
+)
+
+cd %oldDir%
+endlocal
