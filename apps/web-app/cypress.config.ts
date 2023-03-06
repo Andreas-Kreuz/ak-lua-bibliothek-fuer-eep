8b411040c60c92898861222d55b6ffc013f3d361
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2023-03-06 18:43:37 +0100

    use preview build for tests

--- apps/web-app/cypress.config.ts
+++ apps/web-app/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'g5rj4e',
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4173',
     testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
