98c6823c316e95e60962b6b2c481481df121b2d8
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2025-03-30 11:45:28 +0200

    fix different card heights in single row

--- apps/web-app/src/ui/AppCardGrid.tsx
+++ apps/web-app/src/ui/AppCardGrid.tsx
@@ -3,7 +3,7 @@ import { Key, ReactNode } from 'react';
 
 function AppCardGrid(props: { reactKey?: Key; children: ReactNode }) {
   return (
-    <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={props.reactKey}>
+    <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} container key={props.reactKey}>
       {props.children}
     </Grid>
   );
