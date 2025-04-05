444d7b8126b10bfafb5d7829b776a3587ed842b1
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2025-04-05 23:34:18 +0200

    set grid to column and stretch

--- apps/web-app/src/ui/AppCardGrid.tsx
+++ apps/web-app/src/ui/AppCardGrid.tsx
@@ -3,7 +3,12 @@ import { Key, ReactNode } from 'react';
 
 function AppCardGrid(props: { reactKey?: Key; children: ReactNode }) {
   return (
-    <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} container key={props.reactKey}>
+    <Grid
+      size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+      container
+      key={props.reactKey}
+      sx={{ flexDirection: 'column', alignItems: 'stretch' }}
+    >
       {props.children}
     </Grid>
   );
