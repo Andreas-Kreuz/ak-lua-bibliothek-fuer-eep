be71f6bbef0e28e22520dc3bb6e6f095e936217e
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2025-04-06 12:41:34 +0200

    add desktop.ini to ignored files

--- lua/LUA/ModellInstallation.lua
+++ lua/LUA/ModellInstallation.lua
@@ -15,7 +15,7 @@ local paket0 = AkModellPaket:new("13,2", "Lua-Bibliothek von Andreas Kreuz",
                                  "Lua-Bibliothek mit Verkehrssteuerung, Aufgabenplanung und Modell-Installation")
 paket0:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak", {
     "README.md", "ak-eep-in.commands", "ak-eep-out.json", "ak-eep-out-json.isfinished", "ak-eep-out.log",
-    "ak-eep-out.socket", "ak-eep-version.txt", "ak-server.iswatching", "anlagen"
+    "ak-eep-out.socket", "ak-eep-version.txt", "ak-server.iswatching", "anlagen", "desktop.ini"
 })
 
 do
@@ -28,27 +28,31 @@ end
 -- Paket: Demo-Anlage Ampelkreuzung
 -----------------------------------------
 local paket1 = AkModellPaket:new("13,2", "Demo-Anlage (Ampel, ÖPNV)", "Die Demo-Anlagen für Ampeln und ÖPNV")
-paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\ampel", {"README.md"})
-paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Ampel", {".dds", "README.md"})
-paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\demo-linien", {".dds", "README.md"})
-paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Linien", {".dds", "README.md"})
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\ampel", { "README.md", "desktop.ini" })
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Ampel",
+                         { ".dds", "README.md", "desktop.ini" })
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\demo-linien",
+                         { ".dds", "README.md", "desktop.ini" })
+paket1:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Linien",
+                         { ".dds", "README.md", "desktop.ini" })
 
 -----------------------------------------
 -- Paket: Demo-Anlage testen
 -----------------------------------------
 local paket2 = AkModellPaket:new("13,2", "Demo-Anlage Testen mit EEP (Erweiterte Modelle)",
                                  "Eine Anlage mit Shop-Modellen - mit zwei komplexen Kreuzungen und Ampel-Skripten")
-paket2:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\testen", {"README.md"})
-paket2:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Testen", {".dds", "README.md"})
+paket2:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\testen", { "README.md", "desktop.ini" })
+paket2:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Demo-Testen",
+                         { ".dds", "README.md", "desktop.ini" })
 
 -----------------------------------------
 -- Paket: Tutorial Ampelkreuzung
 -----------------------------------------
 local paket3 = AkModellPaket:new("13,2", "Tutorial - Aufbau einer Ampelkreuzung",
                                  "Eine Anlage mit einer Kreuzung, die die Verwendung der Lua-Bibliothek erklärt")
-paket3:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\tutorial-ampel", {"README.md"})
+paket3:fuegeDateienHinzu(aktuellerOrdner, "", "LUA\\ak\\demo-anlagen\\tutorial-ampel", { "README.md", "desktop.ini" })
 paket3:fuegeDateienHinzu(aktuellerOrdner, "", "Resourcen\\Anlagen\\Andreas_Kreuz-Tutorial-Ampelkreuzung",
-                         {".dds", "README.md"})
+                         { ".dds", "README.md", "desktop.ini" })
 
 print("[#ModellInstallation] " .. paket0.deutscherName)
 print("[#ModellInstallation] " .. paket1.deutscherName)
