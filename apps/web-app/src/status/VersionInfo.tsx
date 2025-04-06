a42404c79a5ac7bd4ff2ba4826336b49ed50ed60
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2025-04-06 12:12:46 +0200

    make width responsive

--- apps/web-app/src/status/VersionInfo.tsx
+++ apps/web-app/src/status/VersionInfo.tsx
@@ -2,19 +2,22 @@ import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import useVersionStatus from './useVersionInfo';
+import Grid from '@mui/material/Grid';
 
 function VersionInfo() {
   const [verApp, verEep, verLua] = useVersionStatus();
 
   return (
-    <div>
-      <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2} m={2}>
-        <Typography>Versionen:</Typography>
-        <Chip label={'App ' + verApp} />
-        <Chip label={'EEP ' + verEep} />
-        <Chip label={'Lua ' + verLua} />
-      </Stack>
-    </div>
+    <>
+      <Grid container style={{ alignItems: 'center' }} justifyContent={'space-between'} mt={2} mb={2}>
+        <Typography mr={2}>Versionen:</Typography>
+        <Stack direction="row" justifyContent="flex-start" alignItems="center" spacing={2} mr={2}>
+          <Chip label={'App ' + verApp} />
+          <Chip label={'EEP ' + verEep} />
+          <Chip label={'Lua ' + verLua} />
+        </Stack>
+      </Grid>
+    </>
   );
 }
 
