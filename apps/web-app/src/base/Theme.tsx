0e1690f2f36eafda4c5683cbad69e9edb6164b2b
Andreas Kreuz <andreas.kreuz@endlich-zeit-haben.de>
2025-04-12 09:42:46 +0200

    change secondary color

--- apps/web-app/src/base/Theme.tsx
+++ apps/web-app/src/base/Theme.tsx
@@ -3,7 +3,7 @@ import '@fontsource/anek-latin/300.css';
 import '@fontsource/anek-latin/400.css';
 import '@fontsource/anek-latin/500.css';
 import '@fontsource/anek-latin/700.css';
-import { grey, indigo, yellow } from '@mui/material/colors';
+import { grey, indigo, teal } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 const borderRadius = 12;
@@ -17,7 +17,7 @@ export const theme = createTheme({
       main: indigo[700],
     },
     secondary: {
-      main: yellow[800],
+      main: teal[500],
     },
   },
   typography: {
