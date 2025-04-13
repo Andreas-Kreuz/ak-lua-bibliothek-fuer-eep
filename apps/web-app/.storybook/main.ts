f8154c096935d42a0672fff807a45c166064fc2d
Andreas Kreuz <33174238+Andreas-Kreuz@users.noreply.github.com>
2025-04-13 08:57:25 +0200

    remove absolute path resolution

--- apps/web-app/.storybook/main.ts
+++ apps/web-app/.storybook/main.ts
@@ -2,23 +2,16 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 import { join, dirname } from 'path';
 
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')));
-}
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/experimental-addon-test'),
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
   core: {
