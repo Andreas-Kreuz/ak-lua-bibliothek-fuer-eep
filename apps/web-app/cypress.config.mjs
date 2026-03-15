01c765cf38d00fc7822b08ee731fd011e89df1c0
Andreas Kreuz <33174238+Andreas-Kreuz@users.noreply.github.com>
2026-03-15 20:53:04 +0100

    Add cypress.config.mjs for Cypress configuration

--- apps/web-app/cypress.config.mjs
+++ apps/web-app/cypress.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'cypress';
-import { rm } from 'fs';
+import { rm } from 'node:fs';
 
 export default defineConfig({
   projectId: 'g5rj4e',
   e2e: {
     baseUrl: 'http://localhost:3000',
     testIsolation: false,
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       on('task', {
         deleteEepLogFile(fileName) {
           rm(fileName, () => {});
