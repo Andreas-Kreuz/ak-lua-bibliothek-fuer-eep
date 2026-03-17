cef8f4604315816738c6163c5748694a8dc4dde9
Andreas Kreuz <33174238+Andreas-Kreuz@users.noreply.github.com>
2026-03-17 20:38:35 +0100

    remove wrong override

--- apps/web-server/src/server/mod/train/TrainSelector.ts
+++ apps/web-server/src/server/mod/train/TrainSelector.ts
@@ -40,7 +40,6 @@ export class TrainSelector {
 
       const train: Train = {
         ...trainListEntry,
-        trainType: TrainType.Bike, // FIXME
         trackSystem: trainDto.trackSystem,
         rollingStock,
         length: trainDto.length,
